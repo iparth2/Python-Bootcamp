9e5349988e974ab4a0c8de7fccb3ac4aad0530a8
Parth Garg <parthgarg7300@gmail.com>
2023-06-17 06:43:59 +0530

    if file

---
+++ if.py
@@ -0,0 +1,14 @@
+num_1 = input("Enter a Number: ")
+num_2 = input("Enter a Number: ")
+num_1 = int(num_1)
+num_2 = int(num_2)
+sum = num_1 + num_2
+print(sum)
+if(num_1 > num_2):
+        print("num_1 is greater")
+else:
+      print("num_2 is greater")
+
+
+
+      
